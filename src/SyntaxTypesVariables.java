ff60eb1e3bda330b726f11e879698ce82cb2c69a
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-06 10:56:15 -0500

    char

--- src/SyntaxTypesVariables.java
+++ src/SyntaxTypesVariables.java
@@ -3,21 +3,29 @@ public class SyntaxTypesVariables {
     public static void main(String[] args) {
         //JS Variable Declartation
         //let something = value
-        int a = 10;
+        int a = 10;                   //about precision
 
         //JAVA VARIABLE DECLARATION
 
         //DATATYPE -> variableName = value;
 
         byte myByte = 127;
+
         byte daysOfMonth = 31;
 
         short employees = 32767;
 
-        int userID = 1000000000;
+        int userID = 1000000000;      //no suffix
 
         long galaxyCoordinate = 999999999999999999L;
-        System.out.println(galaxyCoordinate);
+        //        System.out.println(galaxyCoordinate);
+
+        float roundedPi = 3.14f; //NEEDS THE f
+
+        double roundedPi2 = 3.141592653589793; //no suffix   //more precision
+
+         char myChar = 'A'; //single character and only time you can use single quotes
+
     }
 
 }
