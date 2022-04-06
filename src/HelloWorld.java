ac6986d048b07eef221e0248c22b1d7226257aac
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-06 14:20:32 -0500

    update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,11 +2,29 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
+
+        int sum = 1 + 1;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+
+        System.out.println(sum); // without new line
+//                        System.out.print(sum); // without new line
+//                        System.out.print(sum); // without new line
+//
+//
+//                        System.out.println(sum); //with new line
+//                        System.out.println(sum);  //with new line
+//                        System.out.println(sum); //with new line
+
+
     }
 
-}
+};
+
 
 //notes
-//TODO: Finish exercise from curriculum below:
+//TODO: Finish exercise for HelloWorld but for curriculm page Syntax...  below:
 
 //
