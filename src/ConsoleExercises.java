45893593ba02aac1a5b6c2622fa6a26a07704d8c
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-06 15:04:44 -0500

    first exercise done

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,12 @@
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+
+        double pi = 3.14159;
+        System.out.println("The value of pi is approximately 3.14."); //check console
+        System.out.printf("The value of pi is approximately %.2f.\n", pi);   //check difference
+        System.out.print("Wifey is the coolest.");
+        // in console
+
+    }
+}
