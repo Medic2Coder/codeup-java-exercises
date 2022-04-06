396aeeba2a99c35d2a1f1c7abdc9eafa3252820d
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-06 10:51:14 -0500

    syntax and variables

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,23 @@
+public class SyntaxTypesVariables {
+
+    public static void main(String[] args) {
+        //JS Variable Declartation
+        //let something = value
+        int a = 10;
+
+        //JAVA VARIABLE DECLARATION
+
+        //DATATYPE -> variableName = value;
+
+        byte myByte = 127;
+        byte daysOfMonth = 31;
+
+        short employees = 32767;
+
+        int userID = 1000000000;
+
+        long galaxyCoordinate = 999999999999999999L;
+        System.out.println(galaxyCoordinate);
+    }
+
+}
