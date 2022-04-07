41eddb127cdbf663b70935d283f927a17dc25f95
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-07 09:17:27 -0500

    solved, but need to verify with instructor

--- src/warmup07april2022.java
+++ src/warmup07april2022.java
@@ -4,12 +4,20 @@ public class warmup07april2022 {
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 // lives in city = first variable boolean
+        boolean livesInCity = true;
 //has transportation = second variable boolean
+        boolean hasTransportation = true;
 //can come in person = boolean expression of previous two variables
-//write if statement, if they can come in person, console writes You can come in person
+        boolean canComeInPerson = livesInCity && hasTransportation;
+        //write if statement, if they can come in person, console writes You can come in person
+        if ( canComeInPerson == true ) {
+            System.out.println("You can come in person");
+        }
+        else if (canComeInPerson == false) {
+            System.out.println("You can't come in person");
+        }
 
     }
-
 
 }
 
