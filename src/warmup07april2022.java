a414ac105e3f33e650d28de917d064dfa0cfb747
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-07 09:27:22 -0500

    jetpacks

--- src/warmup07april2022.java
+++ src/warmup07april2022.java
@@ -4,17 +4,16 @@ public class warmup07april2022 {
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 // lives in city = first variable boolean
-        boolean livesInCity = true;
+        boolean livesInCity = false;
 //has transportation = second variable boolean
-        boolean hasTransportation = true;
+        boolean hasCarTransportation = false;
 //can come in person = boolean expression of previous two variables
-        boolean canComeInPerson = livesInCity && hasTransportation;
+        boolean canComeInPerson = livesInCity && hasCarTransportation;
         //write if statement, if they can come in person, console writes You can come in person
-        if ( canComeInPerson == true ) {
+        if (canComeInPerson == true) {
             System.out.println("You can come in person");
-        }
-        else if (canComeInPerson == false) {
-            System.out.println("You can't come in person");
+        } else if (canComeInPerson == false) {
+            System.out.println("What, you have a jetpack but you still want to attend via zoom?");
         }
 
     }
