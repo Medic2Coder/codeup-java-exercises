5d5b9b800b2d49d0d9cdcb71064cada2c15fa59f
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-07 09:03:36 -0500

    warmup setup

---
+++ warmup07april2022.java
@@ -0,0 +1,10 @@
+public class warmup07april2022 {
+}
+
+//lives in city = first variable boolean
+//has transportation = second variable boolean
+//can come in person = boolean expression of previous two variables
+
+//write if statement, if they can come in person, console writes You can come in person
+
+
