52bba132a91af66ee9bc44201d2dee119428b478
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-07 09:06:13 -0500

    psuedocode

--- src/warmup07april2022.java
+++ src/warmup07april2022.java
@@ -1,10 +1,18 @@
 public class warmup07april2022 {
-}
 
-//lives in city = first variable boolean
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+// lives in city = first variable boolean
 //has transportation = second variable boolean
 //can come in person = boolean expression of previous two variables
-
 //write if statement, if they can come in person, console writes You can come in person
+
+    }
+
+
+}
+
+
 
 
