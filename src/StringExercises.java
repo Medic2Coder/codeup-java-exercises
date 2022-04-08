06620d3172c6d958bf8c32cecd3a5890f6e2fbdb
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-08 10:29:24 -0500

    update

--- src/StringExercises.java
+++ src/StringExercises.java
@@ -6,6 +6,11 @@ public class StringExercises {
                                                 "We don't need no stinking thought control\n";
             System.out.println(output); //thank you classic rock
 
+            String output2 = "Check \"this\" out!, \"s inside of \"s!\n";
+            System.out.println(output2);
+
+            output = "I can do backslashes \\, double backslashes \\\\ triple backslashes \\\\\\.";
+
         }
 
 
