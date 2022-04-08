df5f75403fece80c42d654b3b0398d6e4246c805
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-08 10:26:31 -0500

    update

---
+++ src/StringExercises.java
@@ -0,0 +1,12 @@
+public class StringExercises {
+
+                                 //helped with walkthrough from Casey
+        public static void main(String[] args) {
+            String output = "We don't need no stinking education\n"  +
+                                                "We don't need no stinking thought control\n";
+            System.out.println(output); //thank you classic rock
+
+        }
+
+
+}
