1c8355c131657c37e15732bb7b0aef10ab30b12a
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-13 15:48:06 -0500

    sandbox notes

---
+++ src/Sandbox/Main.java
@@ -0,0 +1,18 @@
+package Sandbox;
+
+public class Main {
+    public static void main(String[] args) {
+          //TODO: Review below:
+        //data type: int, label is studentAge, assignment operator is =, value is 25
+        int studentAge = 25;// Declare and initialize a variable of type int
+        System.out.println(studentAge); // Prints the value of the variable
+        double studentGPA = 3.49;
+        System.out.println(studentGPA);
+        char studentFirstInitial = 'A';
+        System.out.println(studentFirstInitial);
+        char studentLastInitial = 'M';
+        System.out.println(studentLastInitial);
+        boolean hasPerfectAttendance = true; //variable names are greyed out until referenced
+        System.out.println(hasPerfectAttendance); // Prints the value of the variable
+    }
+}
