2229b3c56df5f1bb08297896d8cb0fce20056f6d
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-14 12:24:04 -0500

    update

--- src/JavaTutorialAmazingness/LearningTheThing.java
+++ src/JavaTutorialAmazingness/LearningTheThing.java
@@ -7,13 +7,31 @@ public class LearningTheThing {
         char b = 'a';
         long c = 1234567890123456789L;
         double d = 3.14;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        addExclamationPoint("hot dogs"); //this is a method call, it's a function call
+
+
         //yay you can do so many things with objects!
 
         String name = "Markle"; //semi colons help Java parse it to make sense of it al
         // if it's a different color, like string in this case, it's an object!
         //to do crazy things with the object, just type the objects name and then the dot operator
-        name.toUpperCase();
-        System.out.println(name.toUpperCase());
+        System.out.println(name.toUpperCase()); //toUpperCase() is a method that's part of the string class
+        System.out.println(name.toLowerCase());
+
+        //to start a method... keywords has it's own purpose
     }
+
+        private static void addExclamationPoint(String s) {
+            System.out.println(s + "!"); //to put two strings together,
+            // you need to use the +
+            // operator
+            
+    }
+
+
 
 }
