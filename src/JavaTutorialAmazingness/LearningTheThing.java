861b2b4b99c416b85af68091517505aff5ba7fff
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-14 12:11:09 -0500

    learning the thing has begun

---
+++ src/JavaTutorialAmazingness/LearningTheThing.java
@@ -0,0 +1,19 @@
+package JavaTutorialAmazingness;
+
+public class LearningTheThing {
+
+    public static void main(String[] args) {
+        int a = 5;
+        char b = 'a';
+        long c = 1234567890123456789L;
+        double d = 3.14;
+        //yay you can do so many things with objects!
+
+        String name = "Markle"; //semi colons help Java parse it to make sense of it al
+        // if it's a different color, like string in this case, it's an object!
+        //to do crazy things with the object, just type the objects name and then the dot operator
+        name.toUpperCase();
+        System.out.println(name.toUpperCase());
+    }
+
+}
--- src/Shapes/ShapesTest.java
+++ src/Shapes/ShapesTest.java
@@ -1,9 +1,9 @@
-package Shapes;
-
-public class ShapesTest {
-    public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 40);
-        System.out.println("Area of box1: " + box1.getArea());
-        
-    }
-}
+//package Shapes;
+//
+//public class ShapesTest {
+//    public static void main(String[] args) {
+//        Rectangle box1 = new Rectangle(4, 40);
+//        System.out.println("Area of box1: " + box1.getArea());
+//
+//    }
+//}
