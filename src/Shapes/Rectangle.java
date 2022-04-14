fa0af64b59f7086a707ee0b6e1dd2a2106d65995
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-14 10:50:34 -0500

    update

---
+++ src/Shapes/Rectangle.java
@@ -0,0 +1,21 @@
+package Shapes;
+
+public class Rectangle {
+      protected int width;
+      protected int height;
+
+
+    public static void main(String[] args) {
+        this.length = lengthToSet;
+        this.width = widthToSet;
+
+
+    }
+    public int getArea() {
+        return width * height;
+    }
+
+}
+
+
+////call fields not properties
