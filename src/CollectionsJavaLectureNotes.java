9775ea34d669950f5d65634d229bdb06c7801dc8
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-19 14:27:21 -0500

    update

--- src/CollectionsJavaLectureNotes.java
+++ src/CollectionsJavaLectureNotes.java
@@ -6,17 +6,17 @@ import java.util.Random;
 public class CollectionsJavaLectureNotes {
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        String[] toppings = {"pepperoni", "sausage", "mushrooms", "onions", "green peppers", "pineapple", "extra cheese"};
+
+        List<String> toppingList = new ArrayList<>();
 
         Random random = new Random();
 
-        String[] toppingList = new String[0];
-        int[] toppings = new int[0];
         for (int i = 0; i < 50; i++) {
             toppingList.add(toppings[random.nextInt(toppings.length)]);
         }
 
-        System.out.println(toppingList);
+//        System.out.println(toppingList);
         HashMap<String, Integer> toppingMap = new HashMap<>();
 
         for (String topping : toppingList) {
@@ -29,5 +29,15 @@ public class CollectionsJavaLectureNotes {
             }
 
         }
+
+        System.out.println(toppingMap);
+        System.out.println(toppingMap.get("extra cheese"));
+        System.out.println(toppingMap.get("pineapple"));
+        System.out.println(toppingMap.get("onions"));
+        System.out.println(toppingMap.get("green peppers"));
+        System.out.println(toppingMap.get("mushrooms"));
+        System.out.println(toppingMap.get("sausage"));
+        System.out.println(toppingMap.get("pepperoni"));
+
     }
 }
