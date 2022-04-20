a067d89cced4497c560326a7f5519d883e83dab8
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-20 11:12:31 -0500

    update

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -4,25 +4,79 @@ import java.util.HashMap;
 
 public class GradesApplication {
 
+
     public static void main(String[] args) {
-     HashMap<String, Integer> grades = new HashMap<>();
-     grades.put("A", 90);
-     grades.put("B", 80);
-     grades.put("C", 70);
-     grades.put("D", 60);
-     grades.put("F", 50);
-     grades.put("Excellent!", 100);
+        //HashMap named students with keys of type String and values of type Student Objects with
+        // 4 Student Objects with 3 grades each
 
-//     HashMap<Object, Object> students = new HashMap<>();
-//     students.put("John", 100, 90, 80);
-//     students.put(100, 70, 90, "Mary");
-//     students.put(90, 90, 60, "Bob");
-//     students.put(90, 90, 60, "Jane");
-//     students.put(90, 90, 60, "Sally");
+        HashMap<String, Student> students = new HashMap<>();
+        Student student1 = new Student("John", "gitusername1");
+        student1.addGrade(100);
+        student1.addGrade(90);
+        student1.addGrade(80);
+        Student student2 = new Student("Jane", "gitusername2");
+        student2.addGrade(100);
+        student2.addGrade(70);
+        student2.addGrade(100);
+        Student student3 = new Student("Joe", "gitusername3");
+        student3.addGrade(100);
+        student3.addGrade(60);
+        student3.addGrade(80);
+        Student student4 = new Student("Jack", "gitusername4");
+        student4.addGrade(100);
+        student4.addGrade(90);
+        student4.addGrade(100);
 
-//     System.out.println(students);
-     System.out.println(grades);
+        //Create command line interface prints list of students and their grades
+
+        students.put("gitusername1", student1);
+        students.put("gitusername2", student2);
+        students.put("gitusername3", student3);
+        students.put("gitusername4", student4);
+
+        //ask the user which student they want to see grades for
+        System.out.println("Which student would you like to see grades for?");
+        System.out.println("Enter the git username of the student you would like to see grades " +
+                "for");
+        String gitUsername = System.console().readLine();
+        Student student = null;
+        if (students.containsKey(gitUsername)) {
+            student = students.get(gitUsername);
+            System.out.println("The grades for " + student.getName() + " are: ");
+            for (int i = 0; i < student.getGrades().size(); i++) {
+                System.out.println(student.getGrades().get(i));
+            } //end for loop
+        }
+        else {
+            System.out.println("That student does not exist");
+        } //end if statement
+
+        //print the average grade for the student
+        System.out.println("The average grade for " + student.getName() + " is: " + student.getAverageGrade());
+
     }
 }
+
+
+
+        //ask the user if they want to continue and keep running if they do
+
+
+
+
+//        students = new HashMap<String, Student>();
+//        grades = new HashMap<String, Grade>();
+
+
+//    public static void main(String[] args) {
+//     HashMap<String, Integer> grades = new HashMap<>();
+//     grades.put("A", 90);
+//     grades.put("B", 80);
+//     grades.put("C", 70);
+//     grades.put("D", 60);
+//     grades.put("F", 50);
+//     grades.put("Excellent!", 100);
+//         System.out.println(grades);
+
 
 
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -6,30 +6,10 @@ public class Student {
 
     private double gradeAverage;
 
-
-
-    //    a main method and creating Student objects
-    public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.setName("John");
-        s1.setGrade(90);
-        s1.addGrade(80);
-        s1.addGrade(70);
-        System.out.println(s1.getName() + " " + s1.getGradeAverage());
-
-        Student s2 = new Student();
-        s2.setName("Jane");
-        s2.setGrade(80);
-        s2.addGrade(70);
-        s2.addGrade(60);
-        System.out.println(s2.getName() + " " + s2.getGradeAverage());
-
-        Student s3 = new Student();
-        s3.setName("Joe");
-        s3.setGrade(70);
-        s3.addGrade(60);
-        s3.addGrade(50);
-        System.out.println(s3.getName() + " " + s3.getGradeAverage());
+    public Student(String john, String gitusername) {
+        this.name = john;
+        this.grade = 90;
+        this.gradeAverage = (double) grade / 3;
     }
 
     public String getName() {
@@ -48,19 +28,81 @@ public class Student {
         this.grade = grade;
     }
 
-    public void addGrade (int grade) {
-        this.grade += grade;
-    }
-
     public double getGradeAverage() {
-        return (double) grade / 3;
+        return gradeAverage;
     }
 
     public void setGradeAverage(double gradeAverage) {
         this.gradeAverage = gradeAverage;
     }
 
+    public void addGrade(int i) {
+        this.grade = this.grade + i;
+        this.gradeAverage = (double) this.grade / 3;
+    }
+
+    public String getGrades() {
+        return "Name: " + this.name + "\nGrade: " + this.grade + "\nGrade Average: " + this.gradeAverage;
+    }
 }
+
+
+//    //    a main method and creating Student objects
+//    public static void main(String[] args) {
+//        Student s1 = new Student();
+//        s1.setName("John");
+//        s1.setGrade(90);
+//        s1.addGrade(80);
+//        s1.addGrade(70);
+//        System.out.println(s1.getName() + " " + s1.getGradeAverage());
+//
+//        Student s2 = new Student();
+//        s2.setName("Jane");
+//        s2.setGrade(80);
+//        s2.addGrade(70);
+//        s2.addGrade(60);
+//        System.out.println(s2.getName() + " " + s2.getGradeAverage());
+//
+//        Student s3 = new Student();
+//        s3.setName("Joe");
+//        s3.setGrade(70);
+//        s3.addGrade(60);
+//        s3.addGrade(50);
+//        System.out.println(s3.getName() + " " + s3.getGradeAverage());
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getGrade() {
+//        return grade;
+//    }
+//
+//    public void setGrade(int grade) {
+//        this.grade = grade;
+//    }
+//
+//    public void addGrade (int grade) {
+//        this.grade += grade;
+//    }
+//
+//    public double getGradeAverage() {
+//        return (double) grade / 3;
+//    }
+//
+//    public void setGradeAverage(double gradeAverage) {
+//        this.gradeAverage = gradeAverage;
+//    }
+//
+//    public String getGrades() {
+//        return "";
+//    }
+//}
 
 
 
